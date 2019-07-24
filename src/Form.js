92ea7dd2ce3e6cf2b56be8ec48117e32e4971b12
Jasmine <jasminecharles@hotmail.com>
2019-07-24 13:19:19 -0700

    start of form.js

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+
+
+function Form(){ 
+const [data, setData] = useState( "" );
+return(
+ <form>
+     <fieldset>
+         <legend>Form</legend>
+     </fieldset>
+ </form>
+)
+
+}
+export default Form;
